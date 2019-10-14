29976e7f41986cf99344a5cad35f12a8b89ff5a4
Boeny <boeniareadmirr@gmail.com>
2019-10-15 00:54:56 +0300

    Line2 test wip

--- src/models/Line2.ts
+++ src/models/Line2.ts
@@ -2,7 +2,7 @@ import { Vector2 } from "./Vector2";
 
 export class Line2 {
 
-    protected k: number;
+    protected k: number | null;
     protected b: number;
 
     public static fromVector(v: Vector2): Line2 {
@@ -10,13 +10,13 @@ export class Line2 {
     }
 
     constructor(x1: number, y1: number, x2: number, y2: number) {
-        this.b = (y1 * x2 - y2 * x1) / (x1 - x2);
-        this.k = (y1 - this.b) / x1;
+        this.b = x1 === 0 ? y1 : x2 === 0 ? y2 : (y1 * x2 - y2 * x1) / (x1 - x2);
+        this.k = x1 === 0 && x2 === 0 ? null : x1 === 0 ? (y2 - this.b) / x2 : (y1 - this.b) / x1;
     }
 
     /** returns X if this K !== 0 */
-    public getX(y: number): number {
-        return (y - this.b) / this.k;
+    public getX(y: number): number | null {
+        return this.k === null || this.k === 0 ? null : (y - this.b) / this.k;
     }
 
     public intersection(line: Line2): Vector2 | null {
@@ -24,11 +24,11 @@ export class Line2 {
         if (this.k === line.k) {
             return null;
         }
-        if (this.k !== 0 && line.k === 0) {
-            return new Vector2(this.getX(line.b), line.b);
+        if (this.k && line.k === 0) {
+            return new Vector2(this.getX(line.b)!, line.b);
         }
-        if (this.k === 0 && line.k !== 0) {
-            return new Vector2(line.getX(this.b), this.b);
+        if (this.k === 0 && line.k) {
+            return new Vector2(line.getX(this.b)!, this.b);
         }
         const y = (line.k * this.b / this.k - line.b) / (line.k / this.k - 1);
         return new Vector2(this.getX(y), y);
